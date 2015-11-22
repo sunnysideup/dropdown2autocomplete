d0cb2adf1a96ff9ae1c81b5c4d3e4c2ad8ca49ce
Nicolaas <github@sunnysideup.co.nz>
2015-11-22 22:40:43 +1300

    organising files

---
+++ code/forms/Dropdown2Autoselect.php
@@ -0,0 +1,13 @@
+<?php
+
+
+class Dropdown2Autoselect extends DropdownField {
+
+	function Field() {
+		Requirements::css("dropdown2autoselect/javascript/chosen/chose.min.css");
+		Requirements::javascript(THIRDPARTY_DIR . '/jquery/jquery.js');
+		Requirements::javascript("dropdown2autoselect/javascript/chosen/chosen.jquery.min.js");
+		Return parent::Field();
+	}
+
+}
