17ad5c718ab43bd1271a04d4fbe1fba67f22e775
helpfulrobot <chris@silverstripe.com>
2016-01-01 04:29:55 +1300

    Converted to PSR-2

--- code/forms/Listbox2AutocompleteField.php
+++ code/forms/Listbox2AutocompleteField.php
@@ -7,44 +7,48 @@
  *
  */
   
-class Listbox2AutocompleteField extends ListboxField {
+class Listbox2AutocompleteField extends ListboxField
+{
 
-	/**
-	 * @var boolean
-	 */ 
-	private $autocomplete = true;
+    /**
+     * @var boolean
+     */
+    private $autocomplete = true;
 
-	/**
-	 *
-	 * @return $this
-	 */ 
-	function turnOnAutocomplete() {
-		$this->autocomplete = true;
-		return $this;
-	}
+    /**
+     *
+     * @return $this
+     */
+    public function turnOnAutocomplete()
+    {
+        $this->autocomplete = true;
+        return $this;
+    }
 
-	/**
-	 *
-	 * @return $this
-	 */ 
-	function turnOffAutocomplete() {
-		$this->autocomplete = false;
-		return $this;
-	}
+    /**
+     *
+     * @return $this
+     */
+    public function turnOffAutocomplete()
+    {
+        $this->autocomplete = false;
+        return $this;
+    }
 
-	/**
-	 * @param array $parameters
-	 * @return string
-	 */ 
-	function Field($parameters = array()) {
-		$field = parent::Field($parameters);
-		if($this->autocomplete) {
-			$this->addExtraClass("chosenAutocompleteField");
-			$field = parent::Field($parameters);
-			Requirements::css("dropdown2autocomplete/javascript/chosen/chosen.min.css");
-			Requirements::javascript(THIRDPARTY_DIR . '/jquery/jquery.js');
-			Requirements::javascript("dropdown2autocomplete/javascript/chosen/chosen.jquery.min.js");
-			Requirements::customScript('
+    /**
+     * @param array $parameters
+     * @return string
+     */
+    public function Field($parameters = array())
+    {
+        $field = parent::Field($parameters);
+        if ($this->autocomplete) {
+            $this->addExtraClass("chosenAutocompleteField");
+            $field = parent::Field($parameters);
+            Requirements::css("dropdown2autocomplete/javascript/chosen/chosen.min.css");
+            Requirements::javascript(THIRDPARTY_DIR . '/jquery/jquery.js');
+            Requirements::javascript("dropdown2autocomplete/javascript/chosen/chosen.jquery.min.js");
+            Requirements::customScript('
 					jQuery("#'.$this->ID().'").chosen('.$this->Config()->get("js_settings").');
 					jQuery("body").on(
 						"focus",
@@ -54,11 +58,9 @@ class Listbox2AutocompleteField extends ListboxField {
 						}
 					);
 				',
-				$this->ID()."_chosen_setup"
-			);
-		}
-		return $field;
-		
-	}
-
+                $this->ID()."_chosen_setup"
+            );
+        }
+        return $field;
+    }
 }
