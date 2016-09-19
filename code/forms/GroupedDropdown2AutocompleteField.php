0da516a28ac79d91b218cc6098eea389130b9678
www-data <sunnysideupdevs@gmail.com>
2016-09-19 18:46:19 +1200

    fix ups

--- code/forms/GroupedDropdown2AutocompleteField.php
+++ code/forms/GroupedDropdown2AutocompleteField.php
@@ -6,44 +6,48 @@
  *
  *
  */
-class GroupedDropdown2AutocompleteField extends DropdownField {
+class GroupedDropdown2AutocompleteField extends DropdownField
+{
 
-	/**
-	 * @var boolean
-	 */ 
-	private $autocomplete = true;
+    /**
+     * @var boolean
+     */
+    private $autocomplete = true;
 
-	/**
-	 *
-	 * @return $this
-	 */ 
-	function turnOnAutocomplete() {
-		$this->autocomplete = true;
-		return $this;
-	}
+    /**
+     *
+     * @return $this
+     */
+    public function turnOnAutocomplete()
+    {
+        $this->autocomplete = true;
+        return $this;
+    }
 
-	/**
-	 *
-	 * @return $this
-	 */ 
-	function turnOffAutocomplete() {
-		$this->autocomplete = false;
-		return $this;
-	}
+    /**
+     *
+     * @return $this
+     */
+    public function turnOffAutocomplete()
+    {
+        $this->autocomplete = false;
+        return $this;
+    }
 
-	/**
-	 * @param array $parameters
-	 * @return string
-	 */ 
-	function Field($parameters = array()) {
-		$field = parent::Field($parameters);
-		if($this->autocomplete) {
-			$this->addExtraClass("chosenAutocompleteField");
-			$field = parent::Field($parameters);
-			Requirements::css("dropdown2autocomplete/javascript/chosen/chosen.min.css");
-			Requirements::javascript(THIRDPARTY_DIR . '/jquery/jquery.js');
-			Requirements::javascript("dropdown2autocomplete/javascript/chosen/chosen.jquery.min.js");
-			Requirements::customScript('
+    /**
+     * @param array $parameters
+     * @return string
+     */
+    public function Field($parameters = array())
+    {
+        $field = parent::Field($parameters);
+        if ($this->autocomplete) {
+            $this->addExtraClass("chosenAutocompleteField");
+            $field = parent::Field($parameters);
+            Requirements::css("dropdown2autocomplete/javascript/chosen/chosen.min.css");
+            Requirements::javascript(THIRDPARTY_DIR . '/jquery/jquery.js');
+            Requirements::javascript("dropdown2autocomplete/javascript/chosen/chosen.jquery.min.js");
+            Requirements::customScript('
 					jQuery("#'.$this->ID().'").chosen('.$this->Config()->get("js_settings").');
 					jQuery("body").on(
 						"focus",
@@ -53,11 +57,9 @@ class GroupedDropdown2AutocompleteField extends DropdownField {
 						}
 					);
 				',
-				$this->ID()."_chosen_setup"
-			);
-		}
-		return $field;
-		
-	}
-
+                $this->ID()."_chosen_setup"
+            );
+        }
+        return $field;
+    }
 }
